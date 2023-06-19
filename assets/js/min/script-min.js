981fec1290e557e1a302d625bf25245d0d567f1b
Domenico Flauto <dom.flauto@gmail.com>
2023-06-19 15:28:28 +0100

    blank slate

--- assets/js/min/script-min.js
+++
@@ -1,1 +0,0 @@
-var acc=document.getElementsByClassName("accordion"),i;for(i=0;i<acc.length;i++)acc[i].onclick=function(){this.classList.toggle("active"),this.nextElementSibling.classList.toggle("show")};
