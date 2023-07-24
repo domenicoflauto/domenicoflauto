4f68a8079a2443cd94bb976e00552addbd0b5dc0
Domenico Flauto <dom.flauto@gmail.com>
2023-07-24 01:13:29 +0100

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,82 @@
 import { Analytics } from '@vercel/analytics/react';
 
+import Image from 'next/image';
 import { SocialIcons } from '@/components/socialIcons';
 
 export default function Home() {
   return (
-    <main>
-      <div className="container">
-        <h1 className="title">Hi, I am <i>Dom</i></h1>
+    <Container>
+      <Intro>
+        <h1 className="title">
+          <WrappedText>
+            Hi, I am&nbsp;<Image className='profile-picture' src="/images/dom-bw.png" alt="Dom's Profile picture" width={32} height={32} />
+          </WrappedText>
+          &nbsp;<i>Dom</i></h1>
         <p>
-          I am a product designer based in London, UK.
+          I am a product designer made in&nbsp;
+          <WrappedText>
+            <Image src="/images/flag-it.svg" alt="Flag of Italy" width={18} height={14} />Italy,&nbsp;
+          </WrappedText>
+          <WrappedText>
+            based&nbsp;in
+            <Image src="/images/flag-gb.svg" alt="Flag of the United Kingdom" width={18} height={14} />London,&nbsp;UK.
+          </WrappedText>
         </p>
         <p>
-          I specialise in UX/UI, design systems, user research and prototyping.
-          <br />
-          In the last 8+ years I have helped start-ups growing and becoming<br />more user-centred.
+          In the last 8+ years, I have helped start-ups growing and becoming more user-centred, while putting my heart in UX/UI, design systems, user research and prototyping.
         </p>
         <p>
-          <span className="wrap-dot">
-            <span className="dot" />
-            <span>
-              Open to new opportunities.
-            </span>
-          </span>
-        </p>
-        <p>
-          Do you have an exciting project, work collaboration or idea you want<br />to chat about? I am available for full-time or freelance work.
+          Do you have an exciting project, work collaboration or idea you want to chat about?&nbsp;
+          <WrappedText>I am currently
+            <Dot />
+            <strong>
+              open to new opportunities!
+            </strong>
+          </WrappedText>
           <br />
           Drop me an email at <strong>hello@domenicoflauto.com</strong>
         </p>
         <SocialIcons />
-      </div>
+      </Intro>
+      <Blob />
       <Analytics />
+    </Container>
+  )
+}
+
+function Container({ children, debug = false }: { children: React.ReactNode, debug?: boolean }) {
+  return (
+    <main className="container" style={debug ? { background: "#ffffff15" } : {}}>
+      {children}
     </main>
+  )
+}
+
+function Intro({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="intro">
+      {children}
+    </div>
+  )
+}
+
+
+function Dot() {
+  return (
+    <span className="dot" />
+  );
+}
+
+function Blob() {
+  return (
+    <Image className="blob" src="/images/blob.svg" height={418} width={467} alt="" />
+  )
+}
+
+function WrappedText({ children }: { children: React.ReactNode }) {
+  return (
+    <span className="wrapped-text">
+      {children}
+    </span>
   )
 }
