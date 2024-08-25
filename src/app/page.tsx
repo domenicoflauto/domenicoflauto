8ec05388f414183928f6ca838779fa75d5a95191
Domenico Flauto <18469530+domenicoflauto@users.noreply.github.com>
2024-08-25 23:40:21 +0100

    adds time+locaiton placeholders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,27 @@ import { Work } from '@/components/work';
 import { Footer } from '@/components/footer';
 import { Container } from '@/components/container';
 
+import { Moon, LocationPin } from '@/components/Icon';
+
+import styles from './styles.module.css';
+
 export default function Home() {
   return (
     <>
+      <div className="flex flex-row px-24 gap-3">
+        <div className="flex flex-row justify-center items-center gap-1 py-2">
+          <Moon />
+          <span className={`text-[#F6F6F6] text-xs ${styles.time}`}>
+            00:20 AM GMT
+          </span>
+        </div>
+        <div className="flex flex-row justify-center items-center gap-1 py-2">
+          <LocationPin />
+          <span className={`text-[#F6F6F6] text-xs ${styles.location}`}>
+            London, UK
+          </span>
+        </div>
+      </div>
       {/* <Intro />
       <Work />
       <Footer /> */}
