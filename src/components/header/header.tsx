45090334662c5e7609b072b3c62b940b751a67df
Domenico Flauto <18469530+domenicoflauto@users.noreply.github.com>
2024-08-28 23:43:23 +0100

    minor refactor, tweak mobile nav

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -21,8 +21,6 @@ export function Header() {
     })
   }, [])
 
-  // let sfxUp = new Audio('/sounds/sfx-up.mp3');
-  // let sfxDown = new Audio('/sounds/sfx-down.mp3');
   useKonami({
     // konami code: ↑ ↑ ↓ ↓ ← → ← → b a
     onUnlock: () => {
@@ -30,6 +28,7 @@ export function Header() {
       !konami ? audio?.up?.play() : audio?.down?.play();
     }
   });
+
   return (
     <div className={`fixed px-4 md:px-24 left-0 right-0 top-0 z-20  ${styles["header"]}`}>
       <header className={`py-3 flex sm:flex-row flex-col justify-between`}>
@@ -43,7 +42,7 @@ export function Header() {
             <button
               onClick={() => setIsOpen(!isOpen)}
               type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-200 hover:text-gray-100 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-200 hover:text-gray-100 hover:bg-gray-800 focus:outline-none focus:bg-gray-800"
               aria-controls="mobile-menu"
               aria-expanded={isOpen ? 'true' : 'false'}
             >
@@ -60,38 +59,8 @@ export function Header() {
             </button>
           </div>
         </div>
-        <div className={`${isOpen ? 'block' : 'hidden'} sm:hidden`} id="mobile-menu">
-          <ul className="px-2 pt-2 pb-3 space-y-1 text-[#F6F6F6] text-sm">
-            <li className=''>
-              <a href="#now">Now</a>
-            </li>
-            <li className=''>
-              <a href="#work">Work</a>
-            </li>
-            <li className=''>
-              <a href="#about">About</a>
-            </li>
-            <li className=''>
-              <a href="#about">Contact</a>
-            </li>
-          </ul>
-        </div>
-        <nav className={``}>
-          <ul className={`hidden sm:flex flex-row text-[#F6F6F6] text-sm`}>
-            <li className='px-3'>
-              <a href="#now">Now</a>
-            </li>
-            <li className='px-3'>
-              <a href="#work">Work</a>
-            </li>
-            <li className='px-3'>
-              <a href="#about">About</a>
-            </li>
-            <li className='px-3'>
-              <a href="#about">Contact</a>
-            </li>
-          </ul>
-        </nav>
+        <MobileMenu isOpen={isOpen} />
+        <Nav />
       </header>
       <div className={`${styles.borderGradient}`}></div>
     </div>
@@ -107,5 +76,59 @@ function Logo({ konami }: { konami: boolean }) {
         d="M7.91101 13.5339L12.9069 10.9776C13.5775 10.6345 13.9984 9.94374 13.9959 9.19048L13.9813 4.80838C13.9788 4.06184 13.5607 3.37883 12.897 3.03701L7.91422 0.470836C7.34044 0.175332 6.65932 0.17488 6.08515 0.469623L1.08664 3.03553C0.419503 3.37799 0 4.06488 0 4.81479V9.19507C0 9.94592 0.420557 10.6335 1.08899 10.9755L6.08899 13.5339C6.66106 13.8266 7.33894 13.8266 7.91101 13.5339Z" fill="#EAE7D6" />
       <path d="M5.56137 5.56137L3 7L5.56137 8.43863L7 11L8.43863 8.43863L11 7L8.43863 5.56137L7 3L5.56137 5.56137Z" fill="#090A0B" />
     </svg>
+  )
+}
+
+export function Nav() {
+  return (
+    <nav className={``}>
+      <ul className={`hidden sm:flex flex-row text-[#F6F6F6] text-sm`}>
+        <li className='px-3'>
+          <a href="#now">Now</a>
+        </li>
+        <li className='px-3'>
+          <a href="#work">Work</a>
+        </li>
+        <li className='px-3'>
+          <a href="#about">About</a>
+        </li>
+        <li className='px-3'>
+          <a href="#about">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export function MobileMenu({ isOpen }: { isOpen: boolean }) {
+  return (
+    <div className={`${isOpen ? 'block' : 'hidden'} sm:hidden h-screen`} id="mobile-menu">
+      <ul className="px-2 pt-2 pb-3 space-y-1 text-[#F6F6F6] text-sm">
+        <li className='text-xl pl-3 border-b border-[#222222]'>
+          <a className='flex flex-row justify-between py-3' href="/now">
+            Now
+            <span>→</span>
+          </a>
+        </li>
+        <li className='text-xl pl-3 border-b border-[#222222]'>
+          <a className='flex flex-row justify-between py-3' href="/work">
+            Work
+            <span>→</span>
+          </a>
+        </li>
+        <li className='text-xl pl-3 border-b border-[#222222]'>
+          <a className='flex flex-row justify-between py-3' href="/about">
+            About
+            <span>→</span>
+          </a>
+        </li>
+        <li className='text-xl pl-3 border-b border-[#222222]'>
+          <a className='flex flex-row justify-between py-3' href="/contact">
+            Contact
+            <span>→</span>
+          </a>
+        </li>
+      </ul>
+    </div>
   )
 }
